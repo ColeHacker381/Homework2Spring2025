206c24f2fe6f9229cd55ac2d8c74b75a22484f58
Erfan Esmaeili Fakhabi <er.esmaili2060@gmail.com>
2025-01-24 13:00:15 -0500

    update test

--- testhisto1.py
+++ testhisto1.py
@@ -1,6 +1,6 @@
 from homework2 import histogram
 
-data = [4, 3, 8,4,-1, 2, -10, -5, 0]
-input_dictionary = {'data': data, 'n': 10, 'min_val': -5, 'max_val': 10}
-output = histogram(input_dictionary)
-print(output)
+data = [-2, -2.2, 0, 5.6, 8.3, 10.1, 30, 4.4, 1.9, -3.3, 9, 8]
+input_dictionary = {'data': data, 'n': 15, 'min_val': -5, 'max_val': 10}
+hist = histogram(input_dictionary)
+print(hist)
